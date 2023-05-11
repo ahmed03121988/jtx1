f73e91a2d18bc7e2ef817f4c838a2b6051c849be
ahmed chaouch <chaouchhedi1@gmail.com>
2023-05-12 00:02:23 +0100

    first commit

---
+++ Profile/Adress.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Adress() {
+  return (
+    <div>
+        <h1> 5090 bekalta</h1>
+    </div>
+  )
+}
+
---
+++ Profile/FullName.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FullName() {
+  return (
+    <div>
+        <h1>first name:ahmed</h1>
+        <h2>last name:chaouch</h2>
+        
+    </div>
+  )
+}
+
+export default FullName
